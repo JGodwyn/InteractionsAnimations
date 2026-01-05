1744b54dd88e1370da6bf4870a7be077a79dcaaa
Godwin John <jon.godwynn@gmail.com>
2026-01-05 06:14:38 +0100

    Initial Commit

---
+++ InteractionsAnimations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InteractionsAnimations
+//
+//  Created by Gdwn16 on 05/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InteractionsAnimations/InteractionsAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractionsAnimationsApp.swift
+//  InteractionsAnimations
+//
+//  Created by Gdwn16 on 05/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct InteractionsAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
